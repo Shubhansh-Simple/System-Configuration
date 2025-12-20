3e182ad644c5f522f7297de28e1e6f23a954de2f
Shubhanshu <32585229+Shubhansh-Simple@users.noreply.github.com>
2025-12-20 23:01:16 +0530

    Script to delete default vim themes

---
+++ delete_extra_themes.py
@@ -0,0 +1,29 @@
+import os
+
+delete_theme_db = {
+    "blue.vim" : 1,
+    "darkblue.vim" : 1,
+    "default.vim" : 1,
+    "delek.vim" : 1,
+    "desert.vim" : 1,
+    "elflord.vim" : 1,
+    "evening.vim" : 1,
+    "industry.vim" : 1,
+    "koehler.vim" : 1,
+    "lunaperche.vim" : 1,
+    "morning.vim" : 1,
+    "murphy.vim" : 1,
+    "pablo.vim" : 1,
+    "peachpuff.vim" : 1,
+    "ron.vim" : 1,
+    "shine.vim" : 1,
+    "slate.vim" : 1,
+    "torte.vim" : 1,
+    "zellner.vim" : 1,
+}
+
+for each_theme in os.listdir():
+    if each_theme.endswith(".vim") and \
+            delete_theme_db.get(each_theme):
+            os.remove(each_theme)
+            print("Deleted Theme - ",each_theme)
